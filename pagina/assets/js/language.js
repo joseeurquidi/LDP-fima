edac248fbecbc2e8b6f04fe707a15ee27ca35180
joseeurquidi <josee.urquidi@gmail.com>
2024-01-05 01:22:35 -0600

    Lenguaje es en index

---
+++ pagina/assets/js/language.js
@@ -0,0 +1,27 @@
+document.getElementById("current_year").innerHTML = (new Date).getFullYear()
+let language = window.localStorage.getItem("language")
+if(language == null){
+  language = "es"
+  window.localStorage.setItem("language", "es")
+}
+setLanguage(language)
+
+function setLanguage(thisLanguage){
+  let language_button = document.getElementsByClassName("language-button")
+  for(let i = 0; i < language_button.length; i++){
+    language_button[i].classList.remove("accent-color")
+  }
+  document.getElementById("language-button-" + thisLanguage).classList.add("accent-color")
+
+  language = thisLanguage
+  window.localStorage.setItem("language", thisLanguage)
+
+  let elementsLanguage = document.querySelectorAll("[data-laguange]")
+  for( let i = 0; i < elementsLanguage.length; i++){
+    if( language == "es"){
+      elementsLanguage[i].innerHTML = elementsLanguage[i].dataset.es
+    }else if ( language == "en"){
+      elementsLanguage[i].innerHTML = elementsLanguage[i].dataset.en
+    }
+  }
+}
